d15055553943e766626790ec948e9024bbc322cc
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2022-06-13 17:57:07 +0000

    revisão spring

--- appspring/src/main/java/appspring/Application.java
+++ appspring/src/main/java/appspring/Application.java
@@ -1,8 +1,9 @@
+//Centro da aplicação (main: método principal)
 package appspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //porta de entrada ("comece por aqui")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- appspring/src/main/java/appspring/controllers/HomeController.java
+++ appspring/src/main/java/appspring/controllers/HomeController.java
@@ -1,14 +1,14 @@
 package appspring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model; 
+import org.springframework.web.bind.annotation.RequestMapping; //Define a url que quando for requisitada chamara o metodo
+import org.springframework.ui.Model; //passa dados do controlador para a visualização (jsp)
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("mensagem", "Olá Mnundo!");
-        return "/home/index";
+        return "/home/index"; //caminho a partir do webapp por isso :"/home/index" 
     }
 }
